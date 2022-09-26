daaee073fc07147f37a7b8d635a255d6dd197e1c
ExcpOccured <danilworkpage69@gmail.com>
2022-09-27 02:13:14 +0300

    Get currencies query

---
+++ src/CurrencyObserver/Handlers/Internal/GetCurrenciesFromCbrApiHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Immutable;
+using CurrencyObserver.Common.Clients;
+using CurrencyObserver.Common.Mapping;
+using CurrencyObserver.Common.Models;
+using CurrencyObserver.Queries;
+using JetBrains.Annotations;
+using MediatR;
+
+namespace CurrencyObserver.Handlers.Internal;
+
+[UsedImplicitly]
+public class GetCurrenciesFromCbrApiHandler : IRequestHandler<CurrenciesFiltrationQuery, IReadOnlyList<Currency>>
+{
+    private readonly ICbrClient _cbrClient;
+
+    private readonly IMapper _mapper;
+
+    public GetCurrenciesFromCbrApiHandler(
+        ICbrClient cbrClient,
+        IMapper mapper)
+    {
+        _cbrClient = cbrClient;
+        _mapper = mapper;
+    }
+
+    public async Task<IReadOnlyList<Currency>> Handle(
+        CurrenciesFiltrationQuery query,
+        CancellationToken cancellationToken)
+    {
+        var currenciesFromCbrApi = await _cbrClient.GetCurrencyQuotesAsync(cancellationToken);
+
+        if (currenciesFromCbrApi is null)
+        {
+            return ArraySegment<Currency>.Empty;
+        }
+
+        var cbrCurrenciesDate = currenciesFromCbrApi.Date;
+        return currenciesFromCbrApi
+            .Currencies
+            .Select(currencyFromCbrApi => _mapper.Map(cbrCurrenciesDate, currencyFromCbrApi))
+            .Where(query.FiltrationPredicate)
+            .ToImmutableList();
+    }
+}
---
+++ src/CurrencyObserver/Queries/CurrenciesFiltrationQuery.cs
@@ -0,0 +1,11 @@
+﻿using CurrencyObserver.Common.Models;
+using MediatR;
+
+namespace CurrencyObserver.Queries;
+
+public abstract class CurrenciesFiltrationQuery : IRequest<IReadOnlyList<Currency>>
+{
+    private static bool DefaultFiltrationQuery(Currency currency) => true;
+
+    public Func<Currency, bool> FiltrationPredicate { get; set; } = DefaultFiltrationQuery;
+}
